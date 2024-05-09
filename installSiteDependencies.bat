65a28d77b06e56765de528da2771b846d680f89e
1RedOne <stowen@microsoft.com>
2024-05-09 13:15:17 -0400

    adding to starup script

--- installSiteDependencies.bat
+++ installSiteDependencies.bat
@@ -1,5 +1,6 @@
 gem install bundler && gem update --system 3.5.6
 gem install jekyll-remote-theme && gem install jekyll-gist
+gem install jekyll-timeago
 gem install "bulma-clean-theme" && gem install jekyll -v 3.8.5 && bundle init
 echo "remeber to togle between local and remote theme if debugging"
 bundle add jekyll -v 3.8.5 --skip-install
